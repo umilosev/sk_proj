b685d293a814880a526d2acb0139112bbe0be07e
umilosev <umilosev12822rn@raf.rs>
2024-11-05 21:16:41 +0100

    postavljanje projekta

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+rootProject.name = "sk_proj"
+include("csvImpl")
+include("csvImpl")
+include("pdfImpl")
+include("excelImpl")
+include("textImpl")
+include("testApp")
+include("spec")
