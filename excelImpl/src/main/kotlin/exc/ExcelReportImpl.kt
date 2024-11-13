415e77d10697de2530c2723ee685390ca4a9b852
umilosev <umilosev12822rn@raf.rs>
2024-11-13 11:07:40 +0100

    izbacio sam komentare iz excel report impl

--- excelImpl/src/main/kotlin/exc/ExcelReportImpl.kt
+++ excelImpl/src/main/kotlin/exc/ExcelReportImpl.kt
@@ -1,5 +1,6 @@
 package exc
 
+
 import org.apache.poi.ss.usermodel.*
 import org.apache.poi.ss.util.CellRangeAddress
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
@@ -10,60 +11,52 @@ class ExcelReportImpl : ReportInterface {
     override val implName: String = "XLS"
     override val supportsFormatting: Boolean = true
 
+
     override fun generateReport(
         data: Map<String, List<String>>,
         destination: String,
         header: Boolean,
         title: String?,
-        summary: String?
+        summary: String?,
     ) {
-        // best option would be separating averages, counts and sums on the start (before adding new rows)
-
         val workbook: Workbook = XSSFWorkbook()
         val sheet: Sheet = workbook.createSheet("Report")
         sheet.defaultColumnWidth = 16
 
-        // Add title if provided
         title?.let {
             val titleRow: Row = sheet.createRow(0)
             val titleCell: Cell = titleRow.createCell(0)
             titleCell.setCellValue(it)
-
-            // Merge title cells
             sheet.addMergedRegion(CellRangeAddress(0, 0, 0, data.size - 1))
-
-            // Create and set title style
             val titleStyle = workbook.createCellStyle().apply {
                 alignment = HorizontalAlignment.CENTER
-                // Import Font class
                 val titleFont: Font = workbook.createFont().apply {
                     bold = true
                     fontHeightInPoints = 18
                 }
-                this.setFont(titleFont)
+                setFont(titleFont)
             }
             titleCell.cellStyle = titleStyle
         }
 
         val keysWithoutCalcs = data.keys.filter { x ->
-            !x.contains("_Average") && !x.contains("_Sum")
-                    && !x.contains("Count")
+            !x.contains("_Average") && !x.contains("_Sum") && !x.contains("Count")
         }
 
-        // Create header row if necessary
         if (header) {
             val headerRow: Row = sheet.createRow(1)
             keysWithoutCalcs.forEachIndexed { index, columnName ->
-                headerRow.createCell(index).setCellValue(columnName)
+                val headerCell = headerRow.createCell(index)
+                headerCell.setCellValue(columnName)
             }
         }
 
-        // Add data rows
         val numRows = data.values.first().size
         for (i in 0 until numRows) {
-            val dataRow: Row = sheet.createRow(if (header) i + 2 else i + 1) // Adjust for header
+            val dataRow: Row = sheet.createRow(if (header) i + 2 else i + 1)
             keysWithoutCalcs.forEachIndexed { index, columnName ->
-                dataRow.createCell(index).setCellValue(data[columnName]?.get(i) ?: "")
+                val cell = dataRow.createCell(index)
+                cell.setCellValue(data[columnName]?.get(i) ?: "")
             }
         }
 
@@ -72,19 +65,12 @@ class ExcelReportImpl : ReportInterface {
         val countRows = populateSummaryRow(sheet, data, numRows + 4 + avgRows + 1 + sumRows + 1, "Count")
 
         summary?.let {
-            val summaryRow: Row = sheet.createRow(numRows + 2) // Place summary after data
+            val summaryRow: Row = sheet.createRow(numRows + 2)
             val summaryCell: Cell = summaryRow.createCell(0)
             summaryCell.setCellValue("Summary: $it")
         }
 
-
-        // Write to the destination file
-        FileOutputStream(destination).use { outputStream ->
-            workbook.write(outputStream)
-        }
-
-
-        // Closing the workbook
+        FileOutputStream(destination).use { outputStream -> workbook.write(outputStream) }
         workbook.close()
     }
 
